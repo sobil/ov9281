1188a20e7ff1e1805d53286e1eb563047bfccc11
Your Name <you@example.com>
2025-02-19 14:15:16 +0000

    wip

--- compile.sh
+++ compile.sh
@@ -1,6 +1,6 @@
 
-rm ov9281-overlay.dts.preprocessed ov9281-overlay.dtbo
+rm ov9281-overlay.preprocessed.dts ov9281-overlay.dtbo
 
-cpp -nostdinc -I include -undef -x assembler-with-cpp ov9281-overlay.dts -o ov9281-overlay.dts.preprocessed
-dtc -I dts -O dtb ov9281-overlay.dts.preprocessed -o ov9281-overlay.dtbo
+cpp -nostdinc -I include -undef -x assembler-with-cpp ov9281-overlay.dts -o ov9281-overlay.preprocessed.dts
+dtc -I dts -O dtb ov9281-overlay.preprocessed.dts -o ov9281-overlay.dtbo
 dtc -I dtb -O dts ./ov9281-overlay.dtbo -o ov9281-overlay-decompiled.dts
